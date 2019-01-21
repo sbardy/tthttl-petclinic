dfc2e138c9a54dfc2b5a312f54886526efe43e04
tthttl@gmail.com <tthttl@gmail.com>
2019-01-21 19:19:23 +0100

    create project

---
+++ src/main/java/com/tthttl/tthttlpetclinic/TthttlPetclinicApplication.java
@@ -0,0 +1,14 @@
+package com.tthttl.tthttlpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TthttlPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TthttlPetclinicApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/tthttl/tthttlpetclinic/TthttlPetclinicApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tthttl.tthttlpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TthttlPetclinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
