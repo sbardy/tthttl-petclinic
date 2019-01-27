0e9e344a674973203780390d69c5ed915dab45f2
tthttl@gmail.com <tthttl@gmail.com>
2019-01-27 20:21:22 +0100

    setting up gateway service with dummy route

--- gateway-service/src/main/java/com/tthttl/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/tthttl/gatewayservice/GatewayServiceApplication.java
@@ -2,13 +2,30 @@ package com.tthttl.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.netty.http.client.HttpClient;
 
+@RestController
 @SpringBootApplication
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
+
+    @Bean
+    public RouteLocator getRouteLocator(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route(predicateSpec -> predicateSpec.path("/get").uri("http://httpbin.org:80"))
+                .build();
+    }
+
+
 
 }
 
